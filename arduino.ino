ffb29527dac43e78b9eb206a782c5e58e50b91bf
Renad Alabdullatif <74248225+renad01@users.noreply.github.com>
2022-07-31 22:01:28 +0300

    Add files via upload

---
+++ arduino.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int servoPosition = 0;
+const int servoPin = 9;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  myservo.attach(servoPin);
+
+  myservo.write(90);
+  Serial.println("start");
+}
+
+void loop()
+{
+
+  if (Serial.available() > 0)
+  {
+    String incomingString = Serial.readString();
+    Serial.print(F("Received by Serial: "));
+    Serial.println(incomingString);
+    if (incomingString.startsWith("right"))
+    {
+      Serial.println(F("Turning Right"));
+      for (servoPosition = 0; servoPosition <= 180; servoPosition++)
+      {
+        myservo.write(servoPosition);
+        delay(15);
+      }
+    }
+    else if (incomingString.startsWith("left"))
+    {
+      Serial.println(F("Turning Left"));
+      for (servoPosition = 180; servoPosition >= 0; servoPosition--)
+      {
+        myservo.write(servoPosition);
+        delay(15);
+      }
+    }
+  }
+}
